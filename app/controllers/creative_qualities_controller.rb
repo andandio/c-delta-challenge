add3afb3c815a00eac57ef5e1b2a21741fa17392
Anders Howerton <anders.e.howerton@gmail.com>
2018-10-24 22:20:20 -0700

    normalized score logic, normalized score added to view

--- app/controllers/creative_qualities_controller.rb
+++ app/controllers/creative_qualities_controller.rb
@@ -1,5 +1,8 @@
 class CreativeQualitiesController < ApplicationController
   def index
     @creative_qualities = CreativeQuality.all
+
+    scores = CreativeQuality.collect_global_scores
+    @scores = CreativeQuality.calculate_normalized_scores(scores)
   end
 end
--- app/models/creative_quality.rb
+++ app/models/creative_quality.rb
@@ -2,4 +2,29 @@ class CreativeQuality < ApplicationRecord
   has_many :question_choices
 
   validates :name, :description, :color, presence: true
+
+  def self.collect_global_scores
+    scores = SurveyResponse.all.map do |survey|
+      survey.calculate_creative_quality_scores
+    end
+  end
+
+  def self.calculate_normalized_scores(scores)
+    tally = { "Purpose" => {raw: 0, max: 0},
+               "Empowerment" => {raw: 0, max: 0},
+               "Collaboration" => {raw: 0, max: 0}
+             }
+
+    scores.each do |score|
+      score.each do |quality|
+        qual_name = quality[:name]
+        tally[qual_name][:raw] += quality[:raw]
+        tally[qual_name][:max] += quality[:max]
+      end
+    end
+
+    tally.each do |k,v|
+      tally[k][:normalized] = (tally[k][:raw].to_f/tally[k][:max] * 100).round
+    end
+  end
 end
